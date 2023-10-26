2acd7b5d893789a09db9711fd70f44099166f0ff
camusatlas <carlos5517068@gmail.com>
2023-10-26 16:59:59 -0500

    Comit 7

--- Pagina_Web_Delosi/Content/JS/Paginacion.js
+++ Pagina_Web_Delosi/Content/JS/Paginacion.js
@@ -1,27 +1,56 @@
 ﻿$(document).ready(function () {
-    $('#datatable_users').DataTable({
+    $('#tablax').DataTable({
         language: {
-            processing: "Tratamiento en curso...",
-            search: "Buscar&nbsp;:",
-            lengthMenu: "Agrupar de _MENU_ items",
-            info: "Mostrando del item _START_ al _END_ de un total de _TOTAL_ items",
-            infoEmpty: "No existen datos.",
-            infoFiltered: "(filtrado de _MAX_ elementos en total)",
-            infoPostFix: "",
+            lengthMenu: "",
+            zeroRecords: "Ningún usuario encontrado",
+            info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+            infoEmpty: "Ningún usuario encontrado",
+            infoFiltered: "(filtrados desde _MAX_ registros totales)",
+            search: "Buscar:",
             loadingRecords: "Cargando...",
-            zeroRecords: "No se encontraron datos con tu busqueda",
-            emptyTable: "No hay datos disponibles en la tabla.",
             paginate: {
                 first: "Primero",
-                previous: "Anterior",
+                last: "Último",
                 next: "Siguiente",
-                last: "Ultimo"
-            },
-            aria: {
-                sortAscending: ": active para ordenar la columna en orden ascendente",
-                sortDescending: ": active para ordenar la columna en orden descendente"
+                previous: "Anterior"
             }
         },
-        lengthMenu: [[6], [6]]
+        ordering: false,
+        lengthMenu: false,
+        dom: 'Bfrtip',
+        buttons: {
+            buttons: [
+                {
+                    extend: 'copy',
+                    text: '<i class="bi bi-clipboard-fill"></i>',
+                    titleAttr: 'Copiar',
+                    className: 'btn btn-secondary'
+                },
+                {
+                    extend: 'excel',
+                    text: '<i class="bi bi-file-earmark-spreadsheet"></i>',
+                    titleAttr: 'Exportar a Excel',
+                    className: 'btn btn-success'
+                },
+                {
+                    extend: 'pdf',
+                    text: '<i class="bi bi-file-earmark-pdf"></i>',
+                    titleAttr: 'Exportar a PDF',
+                    className: 'btn btn-danger'
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="bi bi-printer"></i>',
+                    titleAttr: 'Imprimir',
+                    className: 'btn btn-info'
+                },
+                {
+                    extend: 'colvis',
+                    text: 'Filtrar Columnas'
+                }
+            ]
+        }
+        
+
     });
 });
